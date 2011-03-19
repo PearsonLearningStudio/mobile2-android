1cfe6895d694d3edabdb80636fefde3d080b50e1
Tony Hillerson <tony.hillerson@gmail.com>
2011-03-19 17:41:53 -0600

    New strategy for topic detail: the whole view is a list.

--- src/com/ecollege/android/UserTopicActivity.java
+++ src/com/ecollege/android/UserTopicActivity.java
@@ -1,89 +1,55 @@
 package com.ecollege.android;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import roboguice.inject.InjectExtra;
 import roboguice.inject.InjectResource;
 import roboguice.inject.InjectView;
-import android.content.Context;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.TextView;
 
 import com.ecollege.android.activities.ECollegeListActivity;
+import com.ecollege.android.adapter.ParentAdapterObserver;
+import com.ecollege.android.adapter.ResponseAdapter;
 import com.ecollege.android.util.CacheConfiguration;
-import com.ecollege.api.model.DiscussionResponse;
 import com.ecollege.api.model.DiscussionTopic;
 import com.ecollege.api.model.ResponseCount;
-import com.ecollege.api.model.User;
 import com.ecollege.api.model.UserDiscussionResponse;
 import com.ecollege.api.model.UserDiscussionTopic;
 import com.ecollege.api.services.discussions.FetchDiscussionResponsesForTopic;
-import com.ocpsoft.pretty.time.PrettyTime;
 
 public class UserTopicActivity extends ECollegeListActivity {
 	
 	@InjectExtra(DiscussionsActivity.USER_TOPIC_EXTRA) protected UserDiscussionTopic userTopic;
-	@InjectView(R.id.topic_title_text) protected TextView topicTitleText;
-    @InjectView(R.id.icon) protected ImageView topicIcon;
-    @InjectView(R.id.title_text) protected TextView userTopicTitleText;
-    @InjectView(R.id.description_text) protected TextView descriptionText;
-    @InjectView(R.id.total_response_count_text) protected TextView totalResponseCountText;
-    @InjectView(R.id.unread_response_count_text) protected TextView unreadResponseCountText;
-    @InjectView(R.id.user_response_count_text) protected TextView userResponseCountText;
 	@InjectResource(R.string.d_total_reponses) String totalResponsesFormat;
 	@InjectResource(R.string.d_total_reponse) String totalResponseFormat;
 	@InjectResource(R.string.d_responses_by_you) String responsesByYouFormat;
 	@InjectResource(R.string.d_response_by_you) String responseByYouFormat;
 	@InjectResource(R.string.no_responses) String noResponsesString;
+	@InjectView(R.id.topic_title_text) TextView topicTitleText;
 	
 	protected DiscussionTopic topic;
 	protected ResponseAdapter responseAdapter;
+	private ResponseCount responseCount;
 	public LayoutInflater viewInflater;
-	
-	private static PrettyTime prettyTimeFormatter = new PrettyTime();
+	private UserTopicViewAdapter userTopicAdapter;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.user_topic);
-		viewInflater = getLayoutInflater();
 		topic = userTopic.getTopic();
-		ResponseCount responseCount = userTopic.getChildResponseCounts();
-		String htmlSafeTitle = Html.fromHtml(topic.getTitle()).toString();
-		topicTitleText.setText(htmlSafeTitle);
-		userTopicTitleText.setText(htmlSafeTitle);
-		descriptionText.setText(Html.fromHtml(topic.getDescription()));
+		responseCount = userTopic.getChildResponseCounts();
+		viewInflater = getLayoutInflater();
 		
-		String correctFormat = "%d";
-		if (responseCount.getUnreadResponseCount() == 0) {
-			unreadResponseCountText.setVisibility(View.GONE);
-		} else {
-			unreadResponseCountText.setText(Long.toString(responseCount.getUnreadResponseCount()));
-			unreadResponseCountText.setVisibility(View.VISIBLE);
-		}
-		
-		if (responseCount.getTotalResponseCount() == 0) {
-			totalResponseCountText.setText(noResponsesString);
-		} else {
-			correctFormat = (responseCount.getTotalResponseCount() == 1) ? totalResponseFormat : totalResponsesFormat; 
-			totalResponseCountText.setText(String.format(correctFormat, responseCount.getTotalResponseCount()));
-		}
-		
-		if (responseCount.getPersonalResponseCount() == 0) {
-			userResponseCountText.setVisibility(View.GONE);
-		} else {
-			correctFormat = (responseCount.getPersonalResponseCount() == 1) ? responseByYouFormat : responsesByYouFormat; 
-			userResponseCountText.setText(String.format(correctFormat, responseCount.getPersonalResponseCount()));
-			userResponseCountText.setVisibility(View.VISIBLE);
-		}
+		topicTitleText.setText(Html.fromHtml(topic.getTitle()));
 		
 		loadAndDisplayResponsesForTopic();
 	}
@@ -95,9 +61,13 @@ public class UserTopicActivity extends ECollegeListActivity {
 	private ListAdapter createOrReturnResponseAdapter() {
 		if (responseAdapter == null) {
 			responseAdapter = new ResponseAdapter(this, new ArrayList<UserDiscussionResponse>());
+			if (userTopicAdapter == null) {
+				userTopicAdapter = new UserTopicViewAdapter(responseAdapter);
+			}
+			responseAdapter.setLoading(true);
 			fetchResponsesForTopic(false);
 		}
-		return responseAdapter;
+		return userTopicAdapter;
 	}
 
 	private void fetchResponsesForTopic(boolean reload) {
@@ -115,62 +85,87 @@ public class UserTopicActivity extends ECollegeListActivity {
 			responseAdapter.add(response);
 		}
 		responseAdapter.setNotifyOnChange(true);
+		responseAdapter.setLoading(false);
 		responseAdapter.notifyDataSetChanged();
 		loadAndDisplayResponsesForTopic();
 	}
-
-
-	static class ViewHolder {
-        ImageView icon;
-        TextView titleText;
-        TextView authorText;
-        TextView timeText;
-        TextView totalResponseCountText;
-        TextView unreadResponseCountText;
-        TextView userResponseCountText;
-        TextView descriptionText;
-    }
 	
-	protected class ResponseAdapter extends ArrayAdapter<UserDiscussionResponse> {
+	protected class UserTopicViewAdapter extends BaseAdapter {
+		
+		final int STATIC_VIEWS = 3;
+		final long FAKE_ID = 1000000000;
+		
+		private ResponseAdapter responseAdapter;
+		private ParentAdapterObserver adapterObserver;
+		public boolean isLoading;
 
-		public ResponseAdapter(Context context, List<UserDiscussionResponse> topicList) {
-			super(context, R.layout.user_topic_item, topicList);
+		public UserTopicViewAdapter(ResponseAdapter responseAdapter) {
+			assert(responseAdapter != null);
+			this.responseAdapter = responseAdapter;
+			this.adapterObserver = new ParentAdapterObserver(this);
+			responseAdapter.registerDataSetObserver(adapterObserver);
 		}
 		
+		public void notifyDataSetChanged() {
+			super.notifyDataSetChanged();
+		}
+		
+		@Override public boolean areAllItemsEnabled() { return false; }
+		@Override public int getViewTypeCount() { return responseAdapter.getViewTypeCount() + STATIC_VIEWS; }
+		@Override public boolean hasStableIds() { return responseAdapter.hasStableIds() && true; }
+		@Override public boolean isEnabled(int position) {
+			if (position < STATIC_VIEWS) return false;
+			return responseAdapter.isEnabled(position - STATIC_VIEWS);
+		}
+		@Override public int getItemViewType(int position) {
+			if (position < STATIC_VIEWS) {
+				return getViewTypeCount() - (position + 1);
+			} else {
+				return responseAdapter.getItemViewType(position - STATIC_VIEWS);
+			}
+		}
+		public int getCount() { return responseAdapter.getCount() + STATIC_VIEWS; }
+		public Object getItem(int position) { return (position < STATIC_VIEWS) ? null : responseAdapter.getItem(position - STATIC_VIEWS); }
+		public long getItemId(int position) { return (position < STATIC_VIEWS) ? position + FAKE_ID : responseAdapter.getItemId(position - STATIC_VIEWS); }
+
 		public View getView(int position, View convertView, ViewGroup parent) {
-			ViewHolder holder;
-			
+			switch (position) {
+			case 0 : return getViewForTopic(convertView);
+			case 1 : return getViewForDescription(convertView);
+			case 2 : return getViewForPostItem(convertView);
+			default : return responseAdapter.getView(position - STATIC_VIEWS, convertView, parent);
+			}
+		}
+		
+		public class TopicViewHolder {
+			public TextView topicTitleText;
+			public ImageView topicIcon;
+			public TextView userTopicTitleText;
+			public TextView totalResponseCountText;
+			public TextView unreadResponseCountText;
+			public TextView userResponseCountText;
+		}
+		
+		private View getViewForTopic(View convertView) {
+			TopicViewHolder holder;
 			if (convertView == null) {
+				holder = new TopicViewHolder();
 				convertView = viewInflater.inflate(R.layout.user_topic_item, null);
+				holder.topicIcon = (ImageView)convertView.findViewById(R.id.icon);
+				holder.userTopicTitleText = (TextView)convertView.findViewById(R.id.title_text);
+				holder.totalResponseCountText = (TextView)convertView.findViewById(R.id.total_response_count_text);
+				holder.unreadResponseCountText = (TextView)convertView.findViewById(R.id.unread_response_count_text);
+				holder.userResponseCountText = (TextView)convertView.findViewById(R.id.user_response_count_text);
 				
-				holder = new ViewHolder();
-				holder.icon = (ImageView) convertView.findViewById(R.id.icon);
-				holder.titleText = (TextView) convertView.findViewById(R.id.title_text);
-				holder.timeText = (TextView) convertView.findViewById(R.id.time_text);
-				holder.authorText = (TextView) convertView.findViewById(R.id.author_text);
-				holder.descriptionText = (TextView) convertView.findViewById(R.id.description_text);
-				holder.totalResponseCountText = (TextView) convertView.findViewById(R.id.total_response_count_text);
-				holder.unreadResponseCountText = (TextView) convertView.findViewById(R.id.unread_response_count_text);
-				holder.userResponseCountText = (TextView) convertView.findViewById(R.id.user_response_count_text);
 				convertView.setTag(holder);
 			} else {
-				holder = (ViewHolder) convertView.getTag();
+				holder = (TopicViewHolder)convertView.getTag();
 			}
 			
-			UserDiscussionResponse userResponse = getItem(position);
-			DiscussionResponse response = userResponse.getResponse();
-			ResponseCount responseCount = userResponse.getChildResponseCounts();
-			User author = response.getAuthor();
+			String htmlSafeTitle = Html.fromHtml(topic.getTitle()).toString();
+			holder.userTopicTitleText.setText(htmlSafeTitle);
+			
 			String correctFormat = "%d";
-			
-			holder.timeText.setVisibility(View.VISIBLE);
-			holder.authorText.setVisibility(View.VISIBLE);
-			holder.descriptionText.setVisibility(View.VISIBLE);
-			
-			holder.titleText.setText(Html.fromHtml(response.getTitle()).toString());
-			holder.authorText.setText(author.getFirstName() + " " + author.getLastName());
-			holder.descriptionText.setText(Html.fromHtml(response.getDescription()));
-			holder.timeText.setText(prettyTimeFormatter.format(response.getPostedDate().getTime()));
 			if (responseCount.getUnreadResponseCount() == 0) {
 				holder.unreadResponseCountText.setVisibility(View.GONE);
 			} else {
@@ -194,8 +189,43 @@ public class UserTopicActivity extends ECollegeListActivity {
 			}
 			return convertView;
 		}
-
 		
+		public class ExpandableDescriptionHolder {
+			public TextView descriptionText;
+		}
+		
+		private View getViewForDescription(View convertView) {
+			ExpandableDescriptionHolder holder;
+			if (convertView == null) {
+				holder = new ExpandableDescriptionHolder();
+				convertView = viewInflater.inflate(R.layout.expandable_description_item, null);
+				holder.descriptionText = (TextView) convertView.findViewById(R.id.description_text);
+				convertView.setTag(holder);
+			} else {
+				holder = (ExpandableDescriptionHolder) convertView.getTag();
+			}
+			holder.descriptionText.setText(Html.fromHtml(topic.getDescription()));
+			return convertView;
+		}
+		
+		public class ExpandablePostViewHolder {
+			public TextView contractedTextView;
+		}
+
+		private View getViewForPostItem(View convertView) {
+			ExpandablePostViewHolder holder;
+			if (convertView == null) {
+				holder = new ExpandablePostViewHolder();
+				convertView = viewInflater.inflate(R.layout.expandable_post_response_item, null);
+				holder.contractedTextView = (TextView) convertView.findViewById(R.id.post_response_text);
+				
+				convertView.setTag(holder);
+			} else {
+				holder = (ExpandablePostViewHolder) convertView.getTag();
+			}
+			return convertView;
+		}
+
 	}
 
 }
---
+++ src/com/ecollege/android/adapter/ResponseAdapter.java
@@ -0,0 +1,140 @@
+package com.ecollege.android.adapter;
+
+import java.util.List;
+
+import android.content.Context;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ecollege.android.R;
+import com.ecollege.api.model.DiscussionResponse;
+import com.ecollege.api.model.ResponseCount;
+import com.ecollege.api.model.User;
+import com.ecollege.api.model.UserDiscussionResponse;
+import com.ecollege.api.model.UserDiscussionTopic;
+import com.ocpsoft.pretty.time.PrettyTime;
+
+public class ResponseAdapter extends ArrayAdapter<UserDiscussionResponse> {
+	
+	final private class ViewHolder {
+        ImageView icon;
+        TextView titleText;
+        TextView authorText;
+        TextView timeText;
+        TextView totalResponseCountText;
+        TextView unreadResponseCountText;
+        TextView userResponseCountText;
+        TextView descriptionText;
+    }
+
+	final private PrettyTime prettyTimeFormatter = new PrettyTime();
+	final private String totalResponsesFormat;
+	final private String totalResponseFormat;
+	final private String responsesByYouFormat;
+	final private String responseByYouFormat;
+	final private String noResponsesString;
+	final private LayoutInflater viewInflater;
+	
+	protected boolean loading;
+	
+	public ResponseAdapter(Context context, List<UserDiscussionResponse> topicList) {
+		super(context, R.layout.user_topic_item, topicList);
+		totalResponsesFormat = context.getResources().getString(R.string.d_total_reponses);
+		totalResponseFormat = context.getResources().getString(R.string.d_total_reponse);
+		responsesByYouFormat = context.getResources().getString(R.string.d_responses_by_you);
+		responseByYouFormat = context.getResources().getString(R.string.d_response_by_you);
+		noResponsesString = context.getResources().getString(R.string.no_responses);
+		
+		viewInflater = LayoutInflater.from(context);
+	}
+	
+	public boolean isLoading() {
+		return loading;
+	}
+
+	public void setLoading(boolean loading) {
+		this.loading = loading;
+	}
+
+	@Override public int getViewTypeCount() { return (loading) ? 1 : super.getViewTypeCount(); }
+	@Override public boolean isEnabled(int position) { return (loading) ? false : super.isEnabled(position); }
+	@Override public boolean areAllItemsEnabled() { return (loading) ? false : super.areAllItemsEnabled(); }
+	@Override public int getItemViewType(int position) {
+		if (loading) {
+			return IGNORE_ITEM_VIEW_TYPE;
+		} else {
+			return super.getItemViewType(position);
+		}
+	}
+	public int getCount() { return (loading) ? 1 : super.getCount(); }
+	public UserDiscussionResponse getItem(int position) { return (loading) ? null : super.getItem(position); }
+	public long getItemId(int position) { return (loading) ? 0 : super.getItemId(position); }
+	
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder holder;
+		
+		if (loading) return viewInflater.inflate(R.layout.loading_item, null); 
+		
+		if (convertView == null) {
+			convertView = viewInflater.inflate(R.layout.user_topic_item, null);
+			
+			holder = new ViewHolder();
+			holder.icon = (ImageView) convertView.findViewById(R.id.icon);
+			holder.titleText = (TextView) convertView.findViewById(R.id.title_text);
+			holder.timeText = (TextView) convertView.findViewById(R.id.time_text);
+			holder.authorText = (TextView) convertView.findViewById(R.id.author_text);
+			holder.descriptionText = (TextView) convertView.findViewById(R.id.description_text);
+			holder.totalResponseCountText = (TextView) convertView.findViewById(R.id.total_response_count_text);
+			holder.unreadResponseCountText = (TextView) convertView.findViewById(R.id.unread_response_count_text);
+			holder.userResponseCountText = (TextView) convertView.findViewById(R.id.user_response_count_text);
+			convertView.setTag(holder);
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+		}
+		
+		UserDiscussionResponse userResponse = getItem(position);
+		DiscussionResponse response = userResponse.getResponse();
+		ResponseCount responseCount = userResponse.getChildResponseCounts();
+		User author = response.getAuthor();
+		String correctFormat = "%d";
+		
+		holder.timeText.setVisibility(View.VISIBLE);
+		holder.authorText.setVisibility(View.VISIBLE);
+		holder.descriptionText.setVisibility(View.VISIBLE);
+		
+		holder.titleText.setText(Html.fromHtml(response.getTitle()).toString());
+		holder.authorText.setText(author.getFirstName() + " " + author.getLastName());
+		holder.descriptionText.setText(Html.fromHtml(response.getDescription()));
+		holder.timeText.setText(prettyTimeFormatter.format(response.getPostedDate().getTime()));
+		if (responseCount.getUnreadResponseCount() == 0) {
+			holder.unreadResponseCountText.setVisibility(View.GONE);
+		} else {
+			holder.unreadResponseCountText.setText(Long.toString(responseCount.getUnreadResponseCount()));
+			holder.unreadResponseCountText.setVisibility(View.VISIBLE);
+		}
+		
+		if (responseCount.getTotalResponseCount() == 0) {
+			holder.totalResponseCountText.setText(noResponsesString);
+		} else {
+			correctFormat = (responseCount.getTotalResponseCount() == 1) ? totalResponseFormat : totalResponsesFormat; 
+			holder.totalResponseCountText.setText(String.format(correctFormat, responseCount.getTotalResponseCount()));
+		}
+		
+		if (responseCount.getPersonalResponseCount() == 0) {
+			holder.userResponseCountText.setVisibility(View.GONE);
+		} else {
+			correctFormat = (responseCount.getPersonalResponseCount() == 1) ? responseByYouFormat : responsesByYouFormat; 
+			holder.userResponseCountText.setText(String.format(correctFormat, responseCount.getPersonalResponseCount()));
+			holder.userResponseCountText.setVisibility(View.VISIBLE);
+		}
+		return convertView;
+	}
+
+	
+}
+

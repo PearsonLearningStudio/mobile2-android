423c988d26acc4697babbb154ea5e7a574f7bdf1
Tony Hillerson <tony.hillerson@gmail.com>
2011-04-03 21:11:47 -0600

    First take on dropbox submission detail

--- src/com/ecollege/android/DropboxMessageActivity.java
+++ src/com/ecollege/android/DropboxMessageActivity.java
@@ -1,16 +1,21 @@
 package com.ecollege.android;
 
 import roboguice.inject.InjectExtra;
+import roboguice.inject.InjectResource;
 import roboguice.inject.InjectView;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.ecollege.android.activities.ECollegeDefaultActivity;
 import com.ecollege.api.ECollegeClient;
+import com.ecollege.api.model.Course;
 import com.ecollege.api.model.DropboxMessage;
 import com.ecollege.api.services.dropbox.FetchDropboxMessage;
 import com.google.inject.Inject;
+import com.ocpsoft.pretty.time.PrettyTime;
 
 public class DropboxMessageActivity extends ECollegeDefaultActivity {
 	@Inject ECollegeApplication app;
@@ -18,14 +23,27 @@ public class DropboxMessageActivity extends ECollegeDefaultActivity {
 	@InjectExtra("courseId") long courseId;
 	@InjectExtra("basketId") long basketId;
 	@InjectExtra("messageId") long messageId;
-	@InjectView(R.id.main_text) TextView mainText;
+	@InjectView(R.id.course_title_text) TextView courseTitleText;
+	@InjectView(R.id.message_title_text) TextView messageTitleText;
+	@InjectView(R.id.comments_text) TextView commentsText;
+	@InjectView(R.id.message_text) TextView messageText;
+	@InjectView(R.id.author_text) TextView authorText;
+	@InjectView(R.id.date_text) TextView dateText;
+	@InjectView(R.id.view_all_button) Button viewAllButton;
+	@InjectResource(R.string.no_comments) String no_comments;
 	
 	protected ECollegeClient client;
+	protected Course course;
+	protected DropboxMessage message;
+	
+	private static PrettyTime prettyTimeFormatter = new PrettyTime();
 	
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dropbox_message);
         client = app.getClient();
+    	course = app.getCourseById(courseId);
+    	displayResults();
         fetchData();
     }
     
@@ -34,16 +52,19 @@ public class DropboxMessageActivity extends ECollegeDefaultActivity {
     }
     
     public void onServiceCallSuccess(FetchDropboxMessage service) {
-    	DropboxMessage message = service.getResult(); 
-    	
-    	StringBuilder content = new StringBuilder();
-    	
-    	for (int i=0;i<message.getAttachments().size();i++) {
-    		content.append("Attachment[" + i + "]: " + message.getAttachments().get(i).getName() + "\n");
-    	}
-    	
-    	content.append("Comments: " + message.getComments());
-    	
-    	mainText.setText(content);    	
+    	message = service.getResult(); 
+    	displayResults();
     }
+
+	private void displayResults() {
+		if (null != course) {
+			courseTitleText.setText(Html.fromHtml(course.getTitle()));
+		}
+		
+		if (null != message) {
+			authorText.setText(message.getAuthor().getFirstName() + " " + message.getAuthor().getLastName());
+			commentsText.setText(Html.fromHtml(message.getComments()));
+			dateText.setText(prettyTimeFormatter.format(message.getDate().getTime()));
+		}
+	}
 }

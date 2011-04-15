c81e5a41180d605b4e4ca23a447fac1940f95261
Tony Hillerson <tony.hillerson@gmail.com>
2011-04-15 14:29:15 -0600

    Adding the concept of a scope to the file cache.

--- src/com/ecollege/android/util/FileCacheManager.java
+++ src/com/ecollege/android/util/FileCacheManager.java
@@ -10,9 +10,9 @@ import java.util.Calendar;
 import java.util.Iterator;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
+import org.apache.commons.io.FileUtils;
 
 import android.content.Context;
-import android.net.Uri;
 
 import com.ecollege.api.ECollegeHttpResponseCache;
 import com.ecollege.api.ECollegeHttpResponseHandler;
@@ -33,30 +33,23 @@ public class FileCacheManager implements ECollegeHttpResponseCache {
 		this.expirationInMillis = expirationInMillis;
 	}
 
-	public void addToCache(Uri uri, String value) {
-
-	}
-
-	public void invalidateCacheEntry(File file) {
-		synchronized (cacheDir) {
-			file.delete();
-			file = null;
-		}
-	}
-
-	public CacheEntry get(String cacheKey) {
+	public CacheEntry get(String cacheScope, String cacheKey) {
 		File cacheFile = null;
 		long lastModified = Calendar.getInstance().getTimeInMillis();
 		synchronized (cacheDir) {
-			cacheFile = new File(cacheDir, cacheKey);
+			File cacheScopeDirectory = findOrCreateDirectoryForScope(cacheScope);
+			cacheFile = new File(cacheScopeDirectory, cacheKey);
 			
-			if (cacheFile.exists()) {
-				lastModified = cacheFile.lastModified();
-				long now = Calendar.getInstance().getTimeInMillis();
-				
-				if (now - lastModified > expirationInMillis) {
-					invalidateCacheEntry(cacheFile);
-					cacheFile = null;
+			synchronized (cacheScopeDirectory) {
+				if (cacheFile.exists()) {
+					lastModified = cacheFile.lastModified();
+					long now = Calendar.getInstance().getTimeInMillis();
+					
+					if (now - lastModified > expirationInMillis) {
+						invalidateCacheEntry(cacheFile);
+						deleteCacheSubdirectoryIfEmpty(cacheScopeDirectory);
+						cacheFile = null;
+					}
 				}
 			}
 		}
@@ -85,30 +78,57 @@ public class FileCacheManager implements ECollegeHttpResponseCache {
 		return null;
 	}
 
-	public void put(String cacheKey, String responseContent) {
+	public void put(String cacheScope, String cacheKey, String responseContent) {
 		synchronized (cacheDir) {
-			File cacheFile = new File(cacheDir, cacheKey);
-			GZIPOutputStream out = null;
-			try {
-				out = new GZIPOutputStream(new FileOutputStream(cacheFile));
-				out.write(responseContent.getBytes());
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} finally {
+			File cacheScopeDirectory = findOrCreateDirectoryForScope(cacheScope);
+			synchronized (cacheScopeDirectory) {
+				File cacheFile = new File(cacheScopeDirectory, cacheKey);
+				GZIPOutputStream out = null;
 				try {
-					if (out != null) {
-						out.finish();
-						out.close();
-					}
+					out = new GZIPOutputStream(new FileOutputStream(cacheFile));
+					out.write(responseContent.getBytes());
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
 				} catch (IOException e) {
 					e.printStackTrace();
+				} finally {
+					try {
+						if (out != null) {
+							out.finish();
+							out.close();
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
 				}
 			}
 		}
 	}
 
+	public void invalidateCacheScope(String cacheScope) {
+		synchronized (cacheDir) {
+			File cacheScopeDirectory = new File(cacheDir, cacheScope);
+			synchronized (cacheScopeDirectory) {
+				if (cacheScopeDirectory.exists()) {
+					FileUtils.deleteQuietly(cacheScopeDirectory);
+					cacheScopeDirectory = null;
+				}
+			}
+		}
+	}
+	
+	public void invalidateCacheKey(String cacheScope, String cacheKey) {
+		synchronized (cacheDir) {
+			File cacheScopeDirectory = new File(cacheDir, cacheScope);
+			synchronized (cacheScopeDirectory) {
+				if (cacheScopeDirectory.exists()) {
+					File cacheFile = new File(cacheScopeDirectory, cacheKey);
+					invalidateCacheEntry(cacheFile);
+				}
+			}
+		}
+	}
+	
 	public Integer removeInvalidEntries() {
 		File entryFile;
 		long lastModified;
@@ -126,6 +146,47 @@ public class FileCacheManager implements ECollegeHttpResponseCache {
 				}
 			}
 			return counter;
+		}
+	}
+
+	protected void invalidateCacheEntry(File file) {
+		synchronized (cacheDir) {
+			File parentDirectory = file.getParentFile();
+			file.delete();
+			file = null;
+			// if the cache entry's parent directory is not the cacheDirectory itself (which was the case in an old implementation)
+			if (!cacheDir.getAbsolutePath().equals(parentDirectory.getAbsolutePath())) {
+				// delete the parent if empty
+				deleteCacheSubdirectoryIfEmpty(parentDirectory);
+			}
+		}
+	}
+
+	protected File findOrCreateDirectoryForScope(String cacheScope) {
+		synchronized (cacheDir) {
+			File cacheScopeDir = new File(cacheDir, cacheScope);
+			if (!cacheScopeDir.exists()) {
+				try {
+					cacheScopeDir.mkdir();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			return cacheScopeDir;
+		}
+	}
+
+	protected void deleteCacheSubdirectoryIfEmpty(File directory) {
+		synchronized (cacheDir) {
+			synchronized (directory) {
+				if (directory.isDirectory()) {
+					String[] paths = directory.list();
+					if (paths.length == 0) {
+						FileUtils.deleteQuietly(directory);
+						directory = null;
+					}
+				}
+			}
 		}
 	}
 
